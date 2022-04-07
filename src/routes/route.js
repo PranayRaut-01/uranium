1c458967a5cbd21f3263bf32448ed5ce2ff0c811
PranayRaut-01 <pranayraut17@gmail.com>
2022-04-07 19:05:11 +0530

    practice

--- src/routes/route.js
+++ src/routes/route.js
@@ -3,15 +3,32 @@ const logger = require('./logger')
 
 const router = express.Router();
 
-router.get('/test-me', function (req, res) {
-    console.log('------------------')
-    console.log(req)
-    console.log('------------------')
-    console.log('These are the request query parameters: ', req.query)
-    res.send('My first ever api!')
+router.get('/all-candidates', function (req, res) {
+
+const arr = ['pranay','shubham','ruchi',"raghav",'pankaj','suhas',]
+
+    // console.log('------------------')
+    // console.log(req)
+    // console.log('------------------')
+    // console.log('These are the request query parameters: ', req.query)
+ res.send(arr)
 });
 
+router.get('/candidates',function(req,res){
 
+    const arr = ['pranay','shubham','ruchi',"raghav",'pankaj','suhas',]
+    const obj =req.query.count
+    const arr1 =[]
+
+    function count(){
+        for(let i=0; i<obj;i++){
+         arr1.push(arr[i])
+        }
+
+        
+    }
+    res.send(arr1)
+})
 
 
 module.exports = router;
