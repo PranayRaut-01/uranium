21a39e6cedd5240862c5df4ff6bd5322debfede4
PranayRaut-01 <pranayraut17@gmail.com>
2022-04-26 02:24:30 +0530

    axios assignment

--- src/controllers/allController.js
+++ src/controllers/allController.js
@@ -1,100 +1,88 @@
-const userModel = require('../models/userModel')
-const jwt = require('jsonwebtoken');
+const axios = require("axios");
 
-
-   const createUser = async function (req, res) {
-     try{
-       let data = req.body
-       let dataSaved = await userModel.create(data)
-       if(!dataSaved) return    res.status(400).send(  {  msg : "send required data"} )
-       res.status(201).send(  {  msg : dataSaved} )
-     }
-     catch(err){
-      res.status(500).send( {msg:"Error",error:err.message})
-     }
+const getVaccineSession = async function (req, res) {
+  try {
+    let id = req.query.district_id;
+    let date = req.query.date;
+    let options = {
+      method: "get",
+      url: `https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/findByDistrict?district_id=${id}&date=${date}`,
     };
+    let result = await axios(options);
+    let data = result.data;
+    res.status(200).send({ msg: data });
+  } catch (err) {
+    res.status(500).send({ data: err.message });
+  }
+};
 
-   const login = async function (req, res) {
-    try{
-      let userName = req.body.emailId;
-      let password = req.body.password;
-    
-      let user = await userModel.findOne({ emailId: userName, password: password });
-      if (!user)
-        return res.status(400).send({
-          status: false,
-          msg: "username or the password is not valid",
-        });
-      let token = jwt.sign(
-        {
-          userId: user._id.toString(),
-          batch: "Uranium",
-          organisation: "FunctionUp",
-        },
-        "Best-Cohort"
-      );
-      //res.setHeader("x-auth-token", token);
-      res.status(201).send({ status: true, data: token });
-    }
-    catch(err){
-     res.status(500).send( {msg:"Error",error:err.message})
-    }
+const getWhether = async function (req, res) {
+  try {
+    let city = req.query.q;
+    let id = req.query.appid;
+    let options = {
+      method: "get",
+      url: `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${id}`,
     };
+    let result = await axios(options);
+    let data = result.data.main.temp;
+    res.status(200).send({ msg: `The temp is:${data}` });
+  } catch (err) {
+    res.status(500).send({ data: err.message });
+  }
+};
 
-    const getUserData = async function (req, res) {  
-      try{
-      let userId = req.params.userId;
-      let userDetails = await userModel.findById(userId);
-      if (!userDetails)
-        return res.status(400).send({ status: false, msg: "No such user exists" });
-    
-      res.status(200).send({ status: true, data: userDetails });
-    }
-    catch(err){
-     res.status(500).send( {msg:"Error",error:err.message})
-    }
-    };
-
-
-    const updateUser = async function (req, res) {
-      try{
-        let userId = req.params.userId;
-        let user = await userModel.findById(userId);
-       
-        if (!user) {
-          return res.status(400).send("No such user exists");
-        }
-      
-        let userData = req.body;
-        let updatedUser = await userModel.findOneAndUpdate({ _id: userId }, userData,{new:true});
-        res.status(201).send({ status: true, data: updatedUser });
-      }
-      catch(err){
-       res.status(500).send( {msg:"Error",error:err.message})
-      }
+const sortedCities = async function (req, res) {
+  try {
+    let arr = [
+      "Bengaluru",
+      "Mumbai",
+      "Delhi",
+      "Kolkata",
+      "Chennai",
+      "London",
+      "Moscow",
+    ];
+    let arr1 = [];
+    for (let i = 0; i < arr.length; i++) {
+      let object = { city: arr[i] };
+      let options = {
+        method: "get",
+        url: `http://api.openweathermap.org/data/2.5/weather?q=${arr[i]}&appid=3964c8c35f41a6698569a638359e2416`,
       };
+      let result = await axios(options);
+      let data = result.data.main.temp;
+      object.temp = data;
+      arr1.push(object);
+    }
+    arr1.sort((a, b) => a.temp - b.temp);
+    res.status(200).send({ msg: arr1 });
+  } catch {
+    res.status(500).send({ data: err.message });
+  }
+};
 
-      const deleteUser= async function(req,res){
-        try{
-        let userId = req.params.userId;
-        let user = await userModel.findById(userId);
-        if (!user) {
-          return res.status(400).send("No such user exists");
-        }
-        let deletedUser = await userModel.updateOne({ _id: userId }, {isDeleted:true});
-        res.status(201).send({"msg":"your data is deleted"})
-      }
-      catch(err){
-       res.status(500).send( {msg:"Error",error:err.message})
-      }
-      }
-   
+const getMemes = async function (req, res) {
+  try {
+    let id = req.query.username;
+    let pass = req.query.password;
+    let text0 = req.query.text0;
+    let text1 = req.query.text1;
+    let tempId = req.query.template_id;
 
-       module.exports.login=login
-       module.exports.createUser=createUser
-       module.exports.getUserData=getUserData
-       module.exports.updateUser=updateUser
-       module.exports.deleteUser=deleteUser
+    let options = {
+      method: "post",
+      url: `http://api.imgflip.com/caption_image?template_id=${tempId}&password=${pass}&username=${id}&text0=${text0}&text1=${text1}`,
+    };
+    let result = await axios(options);
+    let data = result.data;
+    res.status(200).send({ msg: data });
+  } catch {
+    res.status(500).send({ data: err.message });
+  }
+};
 
-      
-     
+module.exports.getVaccineSession = getVaccineSession;
+module.exports.getWhether = getWhether;
+module.exports.getMemes = getMemes;
+module.exports.sortedCities = sortedCities;
--- src/routes/route.js
+++ src/routes/route.js
@@ -1,14 +1,10 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const userModels = require('../models/userModel')
- const allControllers = require('../controllers/allController')
- const middleware= require('../middleware/auth')
+const allControllers = require("../controllers/allController");
 
+router.get("/cowin/getVaccineSession", allControllers.getVaccineSession);
+router.get("/getWhether", allControllers.getWhether);
+router.post("/getMemes", allControllers.getMemes);
+router.get("/sortedCities", allControllers.sortedCities);
 
-    router.post('/createUser',allControllers.createUser)
-    router.post('/login',allControllers.login)
-    router.get('/users/:userId',middleware.mid1,allControllers.getUserData)
-    router.put('/users/:userId',middleware.mid1,allControllers.updateUser)
-    router.delete('/users/:userId',middleware.mid1,allControllers.deleteUser)
-  
 module.exports = router;
